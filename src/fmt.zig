6f3f94d00a5a70ca5da437fe5d50dedbe87ecb50
Anton Bulakh <him@necauq.ua>
2026-04-07 20:02:07 +0300

    Use std.fmt.Alt for formatters

--- src/fmt.zig
+++ src/fmt.zig
@@ -1,31 +1,23 @@
 const std = @import("std");
 
-pub const Ptr = struct {
-    ptr: usize,
-
-    pub fn format(
-        self: Ptr,
-        writer: *std.Io.Writer,
-    ) std.Io.Writer.Error!void {
-        try writer.print("\x1b[2;35m0x{x:0>8}\x1b[0m", .{self.ptr});
-    }
-};
-
-pub const Str = struct {
-    str: []const u8,
-
-    pub fn format(
-        self: Str,
-        writer: *std.Io.Writer,
-    ) std.Io.Writer.Error!void {
-        try writer.print("\x1b[2;32m\"{s}\"\x1b[0m", .{self.str});
-    }
-};
-
-pub fn ptr(ptr_: usize) Ptr {
-    return .{ .ptr = ptr_ };
+fn formatPtr(
+    self: usize,
+    writer: *std.Io.Writer,
+) std.Io.Writer.Error!void {
+    try writer.print("\x1b[2;35m0x{x:0>8}\x1b[0m", .{self});
 }
 
-pub fn str(str_: []const u8) Str {
-    return .{ .str = str_ };
+pub fn ptr(ptr_: usize) std.fmt.Alt(usize, formatPtr) {
+    return .{ .data = ptr_ };
+}
+
+fn formatStr(
+    self: []const u8,
+    writer: *std.Io.Writer,
+) std.Io.Writer.Error!void {
+    try writer.print("\x1b[2;32m\"{s}\"\x1b[0m", .{self});
+}
+
+pub fn str(str_: []const u8) std.fmt.Alt([]const u8, formatStr) {
+    return .{ .data = str_ };
 }
